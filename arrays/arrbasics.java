47110bb7865cf344dd0a9185ca917b796d01d8eb
Shubham Yadav <159351522+shubhamy2208@users.noreply.github.com>
2025-10-26 18:07:24 +0000

    array basics commit1

---
+++ arrays/arrbasics.java
@@ -0,0 +1,21 @@
+public class arrbasics
+{
+        public static void main(String[] args) {
+            // //direct declaration 
+            // int [] arr = {6,19,7,67,9};
+            // //indexing
+            // System.out.println(arr[1]);// access karne ka tarika
+            // //array ki length check karne ke liye 
+            // System.out.println(arr.length);
+
+
+            int arr[]= new int [5];// 5 length ka array [iska index 0 to 4 tak rahega ]
+            //inalization of array ,array me value dlane ka tarika wo bhi individually 
+            arr[0]=3;
+            arr[1]=7;
+            arr[2]=32;
+            arr[3]=45;
+            arr[4]=4;
+
+        }
+}
