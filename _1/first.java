03a78c8ed966e227fa796864a6f8ed3be7d03788
Shubham Yadav <159351522+shubhamy2208@users.noreply.github.com>
2025-10-26 18:08:39 +0000

    modify basics

--- _1/first.java
+++ _1/first.java
@@ -1,3 +1,4 @@
+package _1;
 public class first{
     public static void main (String[]args){
         System.out.println("happy bunny");
--- _1/print.java
+++ _1/print.java
@@ -1,3 +1,4 @@
+package _1;
 public class print {
     public static void main(String[] args) {
         System.out.println(23);
