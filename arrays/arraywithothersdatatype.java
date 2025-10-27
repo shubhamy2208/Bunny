a71087d23bacc98d432615a96229720ea5219aea
Shubham Yadav <159351522+shubhamy2208@users.noreply.github.com>
2025-10-27 22:25:59 +0000

    also make a array with other data types

---
+++ arrays/arraywithothersdatatype.java
@@ -0,0 +1,14 @@
+public class arraywithothersdatatype {
+    public static void main(String[] args) {
+        // char ch []= new char[8];
+        //  for(int i=0;i>ch.length;i++){
+        //     System.out.print(ch[i]);
+        //  };
+
+
+         String []arr = {"shubham","shub","happy"};
+          for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+         };
+    }
+}
