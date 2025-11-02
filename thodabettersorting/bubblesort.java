88c1678cbbe730b5f380b32b78f85999f22b69c2
Shubham Yadav <159351522+shubhamy2208@users.noreply.github.com>
2025-11-02 20:23:03 +0000

    always yaad rekhna ki swap ka count bhi increase ho isiliye inner loop me swap++ dene ka

--- thodabettersorting/bubblesort.java
+++ thodabettersorting/bubblesort.java
@@ -27,6 +27,7 @@ public class bubblesort {
                     int temp = arr[i];
                     arr[i]= arr[i+1];
                     arr[i+1]=temp;
+                    swap++;
                 }
             }
             if(swap==0) break;
