a561a9275140e555569641fb40e438093b5a16b2
Shubham Yadav <159351522+shubhamy2208@users.noreply.github.com>
2025-11-03 18:29:55 +0000

    first practice in this code find max element in array

--- arrays/findmaxvalueofarray.java
+++ arrays/findmaxvalueofarray.java
@@ -1,14 +1,13 @@
 public class findmaxvalueofarray {
     public static void main(String[] args) {
-        int arr []={45,56,3,4,57,90,23};
-        int max = arr[0];
+        int arr []={45,56,3,4,57,960,23};
+        int max = 0;
         for(int i=0;i<arr.length;i++){
             if(arr[i]>max){
-                max = arr[i];
+                max=arr[i];
             }
-           
         }
-         System.out.print(max);
+        System.out.print(max);
 
     }
 }
