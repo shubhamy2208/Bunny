3818709fc8736de11f44643870395f73ad785071
Shubham Yadav <159351522+shubhamy2208@users.noreply.github.com>
2025-12-27 19:31:15 +0000

    array basic

---
+++ zlivejava/basicsofdsa/arraybasics.java
@@ -0,0 +1,17 @@
+class arraybasics{
+public static void main(String[] args) {
+// int arr[]= {34,45,3,5,6};
+// System.out.println(arr[3]);
+// System.out.println(arr[1]);
+
+int arr[]= new int [5];
+arr[0]=34;
+arr[1]=45;
+arr[2]=3;
+arr[3]=5;
+arr[4]=6;
+System.out.println(arr[1]);
+
+}
+
+}
