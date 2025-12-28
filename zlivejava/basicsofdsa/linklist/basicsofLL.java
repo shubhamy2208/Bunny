d4ff9af1c2f4cb9664d57b58f8d9db9250c1eba8
Shubham Yadav <159351522+shubhamy2208@users.noreply.github.com>
2025-12-28 21:21:30 +0000

    understand ing he next in LL how to join the linklist in dsa

--- zlivejava/basicsofdsa/linklist/basicsofLL.java
+++ zlivejava/basicsofdsa/linklist/basicsofLL.java
@@ -5,11 +5,22 @@ class node{
 
     node(int d){
         this.data = d ;
+        this.next=null;
        
     }
  class basicsofLL {}
     public static void main(String[] args) {
         node n = new node(34);
+        node m = new node(35);
+        node o = new node(36);
+        node p = new node(37);
+        node q = new node(38);
+        node r = new node(39);
+        n.next=m;
+        m.next=o;
+        o.next=p;
+        p.next=q;
+        q.next=r;
        System.out.println("n.data = " + n.data);
        System.out.println("n.data = " + n.next);
     }
