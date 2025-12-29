2f82b7bf6d109ac27f9cc777be53702818e6cdbe
Shubham Yadav <159351522+shubhamy2208@users.noreply.github.com>
2025-12-29 19:47:39 +0000

    try to perfect basic of Linkklist

---
+++ zlivejava/basicsofdsa/linklist/basicofLL.java
@@ -0,0 +1,34 @@
+// ek box banana 
+class Node{
+    int data ;
+    Node next;
+
+    Node(int data){
+        this.data = data;
+        this.next=null;
+    }
+};
+public class basicofLL{
+    public static void main(String[] args) {
+
+        // creating a node 
+        Node a = new Node(10);
+        Node b = new Node(20);
+        Node c = new Node(30);
+        Node d = new Node(40);
+        Node e = new Node(50);
+
+        // linking a node ,connecting a node 
+        a.next=b;
+        b.next=c;
+        c.next=d;
+        d.next=e;
+
+        // print the node and the address or next node 
+
+        System.out.println(a.data);
+        System.out.println(a.next);
+        System.out.println(b.next);
+
+    }
+}
