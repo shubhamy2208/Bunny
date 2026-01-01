cb01acc540531c211fc3e48bfa707eb71414d1db
Shubham Yadav <159351522+shubhamy2208@users.noreply.github.com>
2026-01-01 20:46:50 +0000

    this is the leetcode easy type problem where we find the duplicate value a nd print true

---
+++ zlivejava/basicsofdsa/array/Containsduplicate.java
@@ -0,0 +1,22 @@
+package zlivejava.basicsofdsa.array;
+
+import java.util.HashSet;
+
+public class Containsduplicate {
+    class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        
+        HashSet<Integer> set = new HashSet<>();
+        
+        for(int i=0; i< nums.length; i++){
+            if(set.contains(nums[i])){
+                return true;
+            }
+            else{
+                set.add(nums[i]);
+            }
+        }
+        return false;
+    }
+}
+}
