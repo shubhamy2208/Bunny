d65a87d5e4d17813d57228a953df298be0b7fac9
Shubham Yadav <159351522+shubhamy2208@users.noreply.github.com>
2026-02-14 13:49:48 +0000

    updated two sum method

---
+++ arrays/updatedtwosum.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class updatedtwosum {
+
+    public static int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int need = target - nums[i];
+
+            if (map.containsKey(need)) {
+                return new int[]{ map.get(need), i };
+            }
+            map.put(nums[i], i);
+        }
+        return new int[]{};  // agar pair na mile
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] result = twoSum(nums, target);
+        System.out.println("Indices: " + Arrays.toString(result));
+    }
+}
