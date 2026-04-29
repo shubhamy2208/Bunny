d4d1c1b7238267568c8b369856725f39e592069a
Shubham Yadav <159351522+shubhamy2208@users.noreply.github.com>
2026-04-29 09:40:52 +0000

    second pattern solve dhyan rekhna sout ln ka waha isse aa raha hai

---
+++ patterns/pattern2.java
@@ -0,0 +1,14 @@
+package patterns;
+
+public class pattern2 {
+    public static void main(String arg[]){
+        for(int i=0;i<4;i++){
+            for(int j=0;j<i+1;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+             
+    }
+    }
+    
+}
