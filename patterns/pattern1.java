21d5ec719aae2821213ea56ba6c66c4151cdad83
Shubham Yadav <159351522+shubhamy2208@users.noreply.github.com>
2026-04-29 08:43:48 +0000

    pattern1 solving hai

---
+++ patterns/pattern1.java
@@ -0,0 +1,12 @@
+package patterns;
+
+class pattern1{
+    public static void main(String arg []){
+        for (int i =0;i<4;i++){
+            for(int j =0;j<4;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
