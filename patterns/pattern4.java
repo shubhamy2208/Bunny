56201317763687263a13eac13fb321b2c601980b
Shubham Yadav <159351522+shubhamy2208@users.noreply.github.com>
2026-04-30 10:56:00 +0000

    Fix loop condition in pattern4 and add pattern5 implementation for star printing

--- patterns/pattern4.java
+++ patterns/pattern4.java
@@ -2,7 +2,7 @@ package patterns;
 
 public class pattern4 {
     public static void main(String arg[]){
-         for(int i=1;i<5;i++){
+         for(int i=1;i<=5;i++){
             for(int j=1;j<=i;j++){
                 System.out.print(i);
             }
---
+++ patterns/pattern5.java
@@ -0,0 +1,13 @@
+package patterns;
+
+public class pattern5 {
+    public static void main(String arg[]){
+        for(int i=0;i<4;i++){
+            for (int j=4-i;j>0;j--){
+                System.out.print("*");
+            }
+            System.out.println();
+
+        }
+    }
+}
