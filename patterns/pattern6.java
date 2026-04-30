8f8c3ae7469b1ca788588cce193b50e8cd13326e
Shubham Yadav <159351522+shubhamy2208@users.noreply.github.com>
2026-04-30 11:08:29 +0000

    Initialise zero, in first loop to gain the first number in the output, otherwise, we left the first 0 if we start with 1

--- patterns/pattern6.java
+++ patterns/pattern6.java
@@ -3,8 +3,8 @@ package patterns;
 public class pattern6 {
      public static void main(String arg[]){
         int n =5;
-        for(int i=1;i<n;i++){
-            for (int j=0;j<=n-i;j++){
+        for(int i=0;i<n;i++){
+            for (int j=1;j<=n-i;j++){
                 System.out.print(j);
             }
             System.out.println();
