b41a87c8aaa4bfeded2f5337596ccf4e273fd025
Shubham Yadav <159351522+shubhamy2208@users.noreply.github.com>
2026-06-08 23:47:11 +0000

    Implement merge sort algorithm and update main method to display sorted array

---
+++ Recursion/1.java
@@ -0,0 +1,67 @@
+package Recursion;
+
+ class Main {
+
+    public static void main(String[] args) {
+
+        int[] arr = {5, 2, 8, 4, 1, 6, 7, 3, -5};
+
+        mergeSort(arr);
+
+        for (int ele : arr) {
+            System.out.print(ele + " ");
+        }
+    }
+
+    private static void mergeSort(int[] arr) {
+
+        int n = arr.length;
+
+        if (n == 1)
+            return;
+
+        // Step 1: Create 2 new arrays
+        int[] a = new int[n / 2];
+        int[] b = new int[n - n / 2];
+
+        // Step 2: Copy arr into a and b
+        int idx = 0;
+
+        for (int i = 0; i < a.length; i++) {
+            a[i] = arr[idx++];
+        }
+
+        for (int i = 0; i < b.length; i++) {
+            b[i] = arr[idx++];
+        }
+
+        // Step 3: Recursively sort
+        mergeSort(a);
+        mergeSort(b);
+
+        // Step 4: Merge
+        merge(a, b, arr);
+    }
+
+    public static void merge(int[] a, int[] b, int[] c) {
+
+        int i = 0, j = 0, k = 0;
+
+        while (i < a.length && j < b.length) {
+
+            if (a[i] <= b[j]) {
+                c[k++] = a[i++];
+            } else {
+                c[k++] = b[j++];
+            }
+        }
+
+        while (i < a.length) {
+            c[k++] = a[i++];
+        }
+
+        while (j < b.length) {
+            c[k++] = b[j++];
+        }
+    }
+}
